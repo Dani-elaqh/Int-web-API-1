fc926e3d64b9578ce18aed92441f865b10c840a9
Dani-elaqh <ale.quintanillah@gmail.com>
2021-04-24 19:13:35 +0000

    server complete

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
 const express =  require('express');
 const morgan = require('morgan');
+const multer  =  require('multer');
+const path =  require('path');
+
 //init
 //server for the app
 const app= express();
@@ -7,7 +10,17 @@ const app= express();
 app.set('port', 3000);
 
 //middleware, every petition will pass trhought morgan
-app.use(morgan());
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename (req, file, cb){
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+   
+})
+app.use(multer({storage}).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //server start
 app.listen(app.get('port'), () => {
