06607a889e1ce2cbe6556a043d232cc8fa66cfb2
Dani-elaqh <ale.quintanillah@gmail.com>
2021-04-25 20:26:54 +0000

    post

--- backend/routes/music.js
+++ backend/routes/music.js
@@ -2,7 +2,18 @@
 const { Router } = require('express');
 const router = Router(); //executing 
 
+const Music = require('../models/musica');
 
-router.get('/', (req, res) => res.json({text: 'Hello humans'}));
+
+router.get('/', async (req, res) => {
+    const musics = await Music.find();
+    res.json(musics);
+});
+
+router.post('/', async (req, res) =>{
+    const { artist, album} = req.body;
+    new Music({artit, album })
+    res.send('received')
+})
 
 module.exports = router;
