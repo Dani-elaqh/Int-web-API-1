1b1eb113a706b43bb4220db0bf40482f659c6b72
Dani-elaqh <ale.quintanillah@gmail.com>
2021-05-02 19:27:12 +0000

    remove mongo credentials from production

--- backend/database.js
+++ backend/database.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 //console.log(process.env.MONGODB_URI)
 
-var MONGO = "mongodb+srv://test:p897am4dr3@cluster0.igfyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
         .then((db) => console.log('connected to db'))
         .catch((err) => console.log(err));
     
