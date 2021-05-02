c75defc77272a51eb0d49128bee0ca5df455c205
Dani-elaqh <ale.quintanillah@gmail.com>
2021-05-02 19:06:04 +0000

    test mongo

--- backend/database.js
+++ backend/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-console.log(process.env.MONGODB_URI)
+//console.log(process.env.MONGODB_URI)
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+var MONGO = "mongodb+srv://test:p897am4dr3@cluster0.igfyh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(MONGO, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
         .then((db) => console.log('connected to db'))
         .catch((err) => console.log(err));
     
